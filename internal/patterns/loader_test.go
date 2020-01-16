2c0ece495ef29037ddfc60b8153e2f02f61249c4
Stuart Jansen <sjansen@buscaluz.org>
2020-01-15 18:52:42 -0700

    add more pattern loader test cases

--- internal/patterns/loader_test.go
+++ internal/patterns/loader_test.go
@@ -2,6 +2,7 @@ package patterns
 
 import (
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,110 +11,223 @@ import (
 func TestLoad(t *testing.T) {
 	require := require.New(t)
 
-	const filename = "testdata/core.star"
-	r, err := os.Open(filename)
-	require.NoError(err)
-	actual, err := Load(filename, r)
-	require.NoError(err)
+	for filename, tc := range map[string]struct {
+		expected *Patterns
+	}{
+		"testdata/basic.star": {
+			expected: basicTree,
+		},
+	} {
+		r, err := os.Open(filename)
+		require.NoError(err)
 
-	require.Equal(expectedCoreTree, actual)
+		actual, err := Load(filename, r)
+		require.NoError(err)
+		require.Equal(tc.expected, actual)
+	}
 }
 
 func TestLoadPatterns(t *testing.T) {
 	require := require.New(t)
 
-	const filename = "testdata/core.star"
-	r, err := os.Open(filename)
-	require.NoError(err)
-	actual, err := loadPatterns(filename, r)
-	require.NoError(err)
+	for filename, tc := range map[string]struct {
+		expected []*pattern
+	}{
+		"testdata/basic.star": {
+			expected: basicPatterns,
+		},
+	} {
+		r, err := os.Open(filename)
+		require.NoError(err)
 
-	require.Equal(expectedCorePatterns, actual)
+		actual, err := loadPatterns(filename, r)
+		require.NoError(err)
+		require.Equal(tc.expected, actual)
+	}
 }
 
-var expectedCorePatterns = []*pattern{
-	{
-		id:     "root",
-		slash:  maySlash,
-		prefix: []part{},
-		params: params{
-			dedup:  keepAll,
-			params: map[string]*param{},
-		},
-		tests: map[string]string{
-			"/":         "/",
-			"/rfc3092/": "",
-		},
-	}, {
-		id:    "slash-required",
-		slash: mustSlash,
-		prefix: []part{
-			&plainPart{
-				value: "foo",
-			},
-		},
-		params: params{
-			dedup:  keepFirst,
-			params: map[string]*param{},
-		},
-		tests: map[string]string{
-			"/foo":  "",
-			"/foo/": "/foo/",
-		},
-	}, {
-		id:    "no-final-slash",
-		slash: neverSlash,
-		prefix: []part{
-			&plainPart{
-				value: "bar",
-			},
-		},
-		params: params{
-			dedup:  keepLast,
-			params: map[string]*param{},
-		},
-		tests: map[string]string{
-			"/bar":  "/bar",
-			"/bar/": "",
+var basicPatterns = []*pattern{{
+	id:     "root",
+	slash:  maySlash,
+	prefix: []part{},
+	params: params{
+		dedup:  keepAll,
+		params: map[string]*param{},
+	},
+	tests: map[string]string{
+		"/":         "/",
+		"/rfc3092/": "",
+	},
+}, {
+	id:    "slash-required",
+	slash: mustSlash,
+	prefix: []part{
+		&plainPart{
+			value: "foo",
 		},
 	},
-}
-
-var expectedCoreTree = &Patterns{
-	tree{
-		id:    "root",
-		slash: maySlash,
-		params: params{
-			dedup:  keepAll,
-			params: map[string]*param{},
+	params: params{
+		dedup:  keepFirst,
+		params: map[string]*param{},
+	},
+	tests: map[string]string{
+		"/foo":  "",
+		"/foo/": "/foo/",
+	},
+}, {
+	id:    "no-final-slash",
+	slash: neverSlash,
+	prefix: []part{
+		&plainPart{
+			value: "bar",
 		},
-		children: []*child{
-			{
-				part: &plainPart{
-					value: "foo",
-				},
-				tree: &tree{
-					id:    "slash-required",
-					slash: mustSlash,
-					params: params{
-						dedup:  keepFirst,
-						params: map[string]*param{},
-					},
-				},
-			},
-			{
-				part: &plainPart{
-					value: "bar",
-				},
-				tree: &tree{
-					id:    "no-final-slash",
-					slash: neverSlash,
-					params: params{
-						dedup:  keepLast,
-						params: map[string]*param{},
-					},
-				},
+	},
+	params: params{
+		dedup:  keepLast,
+		params: map[string]*param{},
+	},
+	tests: map[string]string{
+		"/bar":  "/bar",
+		"/bar/": "",
+	},
+}, {
+	id:    "optional-slash",
+	slash: maySlash,
+	prefix: []part{
+		&plainPart{
+			value: "baz",
+		},
+	},
+	params: params{
+		dedup:  keepAll,
+		params: map[string]*param{},
+	},
+	tests: map[string]string{
+		"/baz":  "/baz",
+		"/baz/": "/baz",
+	},
+}, {
+	id:    "regex",
+	slash: maySlash,
+	prefix: []part{
+		&regexPart{
+			regex: regexp.MustCompile("qux"),
+			rewriter: &staticRewriter{
+				value: "quux",
 			},
 		},
 	},
-}
+	params: params{
+		dedup:  keepAll,
+		params: map[string]*param{},
+	},
+	tests: map[string]string{
+		"/qux/": "/quux",
+	},
+}, {
+	id:    "query",
+	slash: maySlash,
+	prefix: []part{
+		&plainPart{
+			value: "search",
+		},
+	},
+	params: params{
+		dedup: keepAll,
+		params: map[string]*param{
+			"q": {
+				remove: false,
+				rewriter: &staticRewriter{
+					value: "X",
+				},
+			},
+			"utf8": {remove: true},
+		},
+	},
+	tests: map[string]string{
+		"/search?utf8=✔":         "/search",
+		"/search/?q=cats":        "/search?q=X",
+		"/search/?q=dogs&utf8=✔": "/search?q=X",
+	},
+}}
+
+var basicTree = &Patterns{tree{
+	id:    "root",
+	slash: maySlash,
+	params: params{
+		dedup:  keepAll,
+		params: map[string]*param{},
+	},
+	children: []*child{{
+		part: &plainPart{
+			value: "foo",
+		},
+		tree: &tree{
+			id:    "slash-required",
+			slash: mustSlash,
+			params: params{
+				dedup:  keepFirst,
+				params: map[string]*param{},
+			},
+		},
+	}, {
+		part: &plainPart{
+			value: "bar",
+		},
+		tree: &tree{
+			id:    "no-final-slash",
+			slash: neverSlash,
+			params: params{
+				dedup:  keepLast,
+				params: map[string]*param{},
+			},
+		},
+	}, {
+		part: &plainPart{
+			value: "baz",
+		},
+		tree: &tree{
+			id:    "optional-slash",
+			slash: maySlash,
+			params: params{
+				dedup:  keepAll,
+				params: map[string]*param{},
+			},
+		},
+	}, {
+		part: &regexPart{
+			regex: regexp.MustCompile("qux"),
+			rewriter: &staticRewriter{
+				value: "quux",
+			},
+		},
+		tree: &tree{
+			id:    "regex",
+			slash: maySlash,
+			params: params{
+				dedup:  keepAll,
+				params: map[string]*param{},
+			},
+		},
+	}, {
+		part: &plainPart{
+			value: "search",
+		},
+		tree: &tree{
+			id:    "query",
+			slash: maySlash,
+			params: params{
+				dedup: keepAll,
+				params: map[string]*param{
+					"q": {
+						remove: false,
+						rewriter: &staticRewriter{
+							value: "X",
+						},
+					},
+					"utf8": {remove: true},
+				},
+			},
+		},
+	}},
+}}
---
+++ internal/patterns/testdata/basic.star
@@ -0,0 +1,92 @@
+add_url("root",
+    path={
+        "prefix": [],
+        "suffix": "/?",
+    },
+    query={
+        "dedup": "never",
+        "params": {},
+    },
+    tests={
+        "/": "/",
+        "/rfc3092/": None,
+    },
+)
+
+add_url("slash-required",
+    path={
+        "prefix": ["foo"],
+        "suffix": "/",
+    },
+    query={
+        "dedup": "first",
+        "params": {},
+    },
+    tests={
+        "/foo": None,
+        "/foo/": "/foo/",
+    },
+)
+
+add_url("no-final-slash",
+    path={
+        "prefix": ["bar"],
+        "suffix": "",
+    },
+    query={
+        "dedup": "last",
+        "params": {},
+    },
+    tests={
+        "/bar": "/bar",
+        "/bar/": None,
+    },
+)
+
+add_url("optional-slash",
+    path={
+        "prefix": ["baz"],
+        "suffix": "/?",
+    },
+    query={
+        "dedup": "never",
+        "params": {},
+    },
+    tests={
+        "/baz": "/baz",
+        "/baz/": "/baz",
+    },
+)
+
+add_url("regex",
+    path={
+        "prefix": [("qux", "quux")],
+        "suffix": "/?",
+    },
+    query={
+        "dedup": "never",
+        "params": {},
+    },
+    tests={
+        "/qux/": "/quux",
+    },
+)
+
+add_url("query",
+    path={
+        "prefix": ["search"],
+        "suffix": "/?",
+    },
+    query={
+        "dedup": "never",
+        "params": {
+          "q": "X",
+          "utf8": None,
+        },
+    },
+    tests={
+        "/search?utf8=✔":         "/search",
+        "/search/?q=cats":        "/search?q=X",
+        "/search/?q=dogs&utf8=✔": "/search?q=X",
+    },
+)
