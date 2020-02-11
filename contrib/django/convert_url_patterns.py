fc4a0dca54afe1b43608c0bfc01c9eca6efb9bd7
Stuart Jansen <sjansen@buscaluz.org>
2020-02-11 07:46:56 -0700

    refactor contrib/django/convert_url_patterns.py

--- contrib/django/convert_url_patterns.py
+++ contrib/django/convert_url_patterns.py
@@ -5,10 +5,10 @@ from django.core.management import BaseCommand
 
 try:
     from django.urls.converters import get_converters
-except:
 
-    def get_converters():
-        raise NotImplementedError()
+    converters = get_converters()
+except:
+    converters = {}
 
 
 NAMED_REGEX_PART = re.compile(r"^\(\?P<(?P<name>[^>]+)>(?P<regex>.*)\)$")
@@ -16,48 +16,20 @@ NAMED_TYPE_PART = re.compile(r"^<((?P<type>[^:]+):)?(?P<name>[^:]+)>$")
 PLAIN_PART = re.compile("[^|.*+?\\\[\](){}]+")
 
 
-TEST_CASES = {
-    "^$": [],
-    "articles/<int:year>/<int:month>/<slug:slug>/": [
-        "articles",
-        (r"[0-9]+", "year"),
-        (r"[0-9]+", "month"),
-        (r"[-a-zA-Z0-9_]+", "slug"),
-    ],
-    "^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$": [
-        "articles",
-        (r"[0-9]{4}", "year"),
-        (r"[0-9]{2}", "month"),
-        (r"[\w-]+", "slug"),
-    ],
-    "^users/(?P<id>[^/]+)": ["users", (r"[^/]+", "id")],
-}
-
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self_test()
 
 
-def parse(pattern):
-    converters = get_converters()
-
-    prefix = []
-    for token in tokenize(pattern):
-        if NAMED_REGEX_PART.match(token):
-            groups = NAMED_REGEX_PART.match(token).groupdict()
-            part = (groups["regex"], groups["name"])
-        elif NAMED_TYPE_PART.match(token):
-            groups = NAMED_TYPE_PART.match(token).groupdict()
-            regex = converters[groups["type"]].regex
-            part = (regex, groups["name"])
-        elif PLAIN_PART.match(token):
-            part = token
+def self_test():
+    for tc, expected in TEST_CASES.items():
+        actual = Pattern("tc", tc)
+        if expected == actual.prefix:
+            print("PASS: {}".format(tc))
         else:
-            part = (token, "TODO")
-        prefix.append(part)
-
-    return prefix
+            print("FAIL: {}".format(tc))
+            print("  expected: {}".format(expected))
+            print("    actual: {}".format(actual.prefix))
 
 
 def tokenize(pattern):
@@ -90,12 +62,89 @@ def tokenize(pattern):
         yield pattern[begin:]
 
 
-def self_test():
-    for tc, expected in TEST_CASES.items():
-        actual = parse(tc)
-        if expected == actual:
-            print("PASS: {}".format(tc))
+class Pattern(object):
+    def __init__(self, handler, pattern):
+        self.handler = handler
+        self.prefix = []
+        self.__parse(pattern)
+
+    def __parse(self, pattern):
+        for token in tokenize(pattern):
+            if self.__match_named_regex(token):
+                continue
+            elif self.__match_named_type(token):
+                continue
+            elif self.__match_plain(token):
+                continue
+            prefix.append(RegexPart(token, "TODO"))
+
+    def __match_named_regex(self, token):
+        m = NAMED_REGEX_PART.match(token)
+        if not m:
+            return False
+        groups = m.groupdict()
+        self.prefix.append(RegexPart(groups["regex"], groups["name"]))
+        return True
+
+    def __match_named_type(self, token):
+        m = NAMED_TYPE_PART.match(token)
+        if not m:
+            return False
+        groups = m.groupdict()
+        if groups.get("type"):
+            regex = converters[groups["type"]].regex
         else:
-            print("FAIL: {}".format(tc))
-            print("  expected: {}".format(expected))
-            print("    actual: {}".format(actual))
+            regex = "[^/]+"
+        self.prefix.append(RegexPart(regex, groups["name"]))
+        return True
+
+    def __match_plain(self, token):
+        m = PLAIN_PART.match(token)
+        if not m:
+            return False
+        self.prefix.append(PlainPart(token))
+        return True
+
+
+class PlainPart(object):
+    def __init__(self, value):
+        self.type = "plain"
+        self.value = value
+
+    def __eq__(self, other):
+        if not type(self) == type(other):
+            return False
+        return self.value == other.value
+
+
+class RegexPart(object):
+    def __init__(self, regex, replacement):
+        self.type = "regex"
+        self.regex = regex
+        self.replacement = replacement
+
+    def __eq__(self, other):
+        if not type(self) == type(other):
+            return False
+        if not self.regex == other.regex:
+            return False
+        return self.replacement == other.replacement
+
+
+TEST_CASES = {
+    "^$": [],
+    "articles/<int:year>/<int:month>/<slug:slug>/": [
+        PlainPart("articles"),
+        RegexPart(r"[0-9]+", "year"),
+        RegexPart(r"[0-9]+", "month"),
+        RegexPart(r"[-a-zA-Z0-9_]+", "slug"),
+    ],
+    "^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$": [
+        PlainPart("articles"),
+        RegexPart(r"[0-9]{4}", "year"),
+        RegexPart(r"[0-9]{2}", "month"),
+        RegexPart(r"[\w-]+", "slug"),
+    ],
+    "groups/<gid>": [PlainPart("groups"), RegexPart(r"[^/]+", "gid")],
+    "^users/(?P<uid>[^/]+)": [PlainPart("users"), RegexPart(r"[^/]+", "uid")],
+}
